2be22f1fe1c6807b1ab0ca9b0f5dd4d2f71f41a1
jason <1449301367@qq.com>
2016-07-06 16:54:07 +0800

    2016/07/06 16:53    提交 jason

--- forum/src/main/java/com/ctrl/forum/ui/activity/store/StorePingjiaDetailActivity.java
+++ forum/src/main/java/com/ctrl/forum/ui/activity/store/StorePingjiaDetailActivity.java
@@ -1,19 +1,23 @@
 package com.ctrl.forum.ui.activity.store;
 
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.util.SparseArray;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.beanu.arad.base.ToolBarActivity;
 import com.ctrl.forum.R;
-import com.ctrl.forum.base.AppToolBarActivity;
+import com.ctrl.forum.base.MyApplication;
 import com.ctrl.forum.ui.fragment.StorePingjiaDetailFragment;
 
 import butterknife.ButterKnife;
@@ -23,7 +27,7 @@ import butterknife.InjectView;
 * 商城评价详情 activity
 * */
 
-public class StorePingjiaDetailActivity extends AppToolBarActivity implements View.OnClickListener{
+public class StorePingjiaDetailActivity extends ToolBarActivity implements View.OnClickListener{
     @InjectView(R.id.iv_back)//返回键
     ImageView iv_back;
     @InjectView(R.id.viewpager_pingjia_detail)//viewpager
@@ -42,6 +46,31 @@ public class StorePingjiaDetailActivity extends AppToolBarActivity implements Vi
 
    SparseArray<Fragment>fragments=new SparseArray<>();
 
+
+    //手指上下滑动时的最小速度
+    private static final int YSPEED_MIN = 1000;
+
+    //手指向右滑动时的最小距离
+    private static final int XDISTANCE_MIN = 50;
+
+    //手指向上滑或下滑时的最小距离
+    private static final int YDISTANCE_MIN = 100;
+
+    //记录手指按下时的横坐标。
+    private float xDown;
+
+    //记录手指按下时的纵坐标。
+    private float yDown;
+
+    //记录手指移动时的横坐标。
+    private float xMove;
+
+    //记录手指移动时的纵坐标。
+    private float yMove;
+
+    //用于计算手指滑动的速度。
+    private VelocityTracker mVelocityTracker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +79,9 @@ public class StorePingjiaDetailActivity extends AppToolBarActivity implements Vi
        // getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
         ButterKnife.inject(this);
         initView();
+         /*禁止横屏*/
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        MyApplication.getInstance().addActivity(this);
     }
 
 
@@ -152,6 +184,74 @@ public class StorePingjiaDetailActivity extends AppToolBarActivity implements Vi
 
         }
     }
+
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        createVelocityTracker(event);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                xDown = event.getRawX();
+                yDown = event.getRawY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if(viewpager_pingjia_detail.getCurrentItem()==0) {
+                    xMove = event.getRawX();
+                    yMove = event.getRawY();
+                    //滑动的距离
+                    int distanceX = (int) (xMove - xDown);
+                    int distanceY = (int) (yMove - yDown);
+                    //获取顺时速度
+                    int ySpeed = getScrollVelocity();
+                    //关闭Activity需满足以下条件：
+                    //1.x轴滑动的距离>XDISTANCE_MIN
+                    //2.y轴滑动的距离在YDISTANCE_MIN范围内
+                    //3.y轴上（即上下滑动的速度）<XSPEED_MIN，如果大于，则认为用户意图是在上下滑动而非左滑结束Activity
+                    if (distanceX > XDISTANCE_MIN && (distanceY < YDISTANCE_MIN && distanceY > -YDISTANCE_MIN) && ySpeed < YSPEED_MIN) {
+                        finish();
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                recycleVelocityTracker();
+                break;
+            default:
+                break;
+        }
+        return super.dispatchTouchEvent(event);
+    }
+
+    /**
+     * 创建VelocityTracker对象，并将触摸界面的滑动事件加入到VelocityTracker当中。
+     *
+     * @param event
+     *
+     */
+    private void createVelocityTracker(MotionEvent event) {
+        if (mVelocityTracker == null) {
+            mVelocityTracker = VelocityTracker.obtain();
+        }
+        mVelocityTracker.addMovement(event);
+    }
+
+    /**
+     * 回收VelocityTracker对象。
+     */
+    private void recycleVelocityTracker() {
+        mVelocityTracker.recycle();
+        mVelocityTracker = null;
+    }
+
+    /**
+     *
+     * @return 滑动速度，以每秒钟移动了多少像素值为单位。
+     */
+    private int getScrollVelocity() {
+        mVelocityTracker.computeCurrentVelocity(1000);
+        int velocity = (int) mVelocityTracker.getYVelocity();
+        return Math.abs(velocity);
+    }
+
 
 
 
