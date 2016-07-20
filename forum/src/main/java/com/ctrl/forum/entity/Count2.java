3463636423840da8295db6ab06e84e13f51be008
jason <1449301367@qq.com>
2016-07-20 17:30:31 +0800

    2016/07/20 17:30  提交 jason

---
+++ forum/src/main/java/com/ctrl/forum/entity/Count2.java
@@ -0,0 +1,44 @@
+package com.ctrl.forum.entity;
+
+/**
+ * 优惠劵-查询总数
+ * Created by Administrator on 2016/5/10.
+ */
+public class Count2 {
+    private String haoping;//好评
+    private String zhongping;//中评
+    private String chaping;//差评
+    private String quanbu;//全部
+
+    public String getHaoping() {
+        return haoping;
+    }
+
+    public void setHaoping(String haoping) {
+        this.haoping = haoping;
+    }
+
+    public String getZhongping() {
+        return zhongping;
+    }
+
+    public void setZhongping(String zhongping) {
+        this.zhongping = zhongping;
+    }
+
+    public String getChaping() {
+        return chaping;
+    }
+
+    public void setChaping(String chaping) {
+        this.chaping = chaping;
+    }
+
+    public String getQuanbu() {
+        return quanbu;
+    }
+
+    public void setQuanbu(String quanbu) {
+        this.quanbu = quanbu;
+    }
+}
